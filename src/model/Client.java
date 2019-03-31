d1eee130188625d374ffda496b7ed80360068c3d
Gabe <geblarot96@gmail.com>
2019-03-30 23:32:18 -0500

    Added some stuff

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,5 +1,11 @@
 package model;
 
-public interface Client {
-
+public interface Client 
+{
+	public String name(String pName, String pLastName); // Client has a name and a last name
+	public int age(int pAge); // Client has an age
+	public int document(int pDocument); // Client has an ID
+	public double score(double pScore); // Client has a Credit Score. ALWAYS
+	public double incomes(double pIncomes); // Client must have an income
+	public int yoe(int pYoE); // Years Of Experience
 }
--- src/model/PremiumClient.java
+++ src/model/PremiumClient.java
@@ -1,5 +1,50 @@
 package model;
 
-public class PremiumClient {
+public class PremiumClient implements Client
+{
+	private String name, lastname;
+	private int age, document, yoe;
+	private double score, income;
 
+	@Override
+	public String name(String pName, String pLastName) 
+	{
+		// TODO Auto-generated method stub
+		return name+" "+lastname;
+	}
+
+	@Override
+	public int age(int pAge) 
+	{
+		// TODO Auto-generated method stub
+		return age;
+	}
+
+	@Override
+	public int document(int pDocument) 
+	{
+		// TODO Auto-generated method stub
+		return document;
+	}
+
+	@Override
+	public double score(double pScore) 
+	{
+		// TODO Auto-generated method stub
+		return score;
+	}
+
+	@Override
+	public double incomes(double pIncomes) 
+	{
+		// TODO Auto-generated method stub
+		return income;
+	}
+
+	@Override
+	public int yoe(int pYoE) 
+	{
+		// TODO Auto-generated method stub
+		return yoe;
+	}
 }
